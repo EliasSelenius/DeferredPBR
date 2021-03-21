d2e6c66eab09b7e3d88137bd23c8a7792a944b0f
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-03-21 19:27:47 +0100

    Canvas.rect()

--- Engine/Gui/Canvas.cs
+++ Engine/Gui/Canvas.cs
@@ -11,6 +11,9 @@ namespace Engine.Gui {
 
         todo:
             - rect
+                - border
+                - border radius
+                - 
             - line
             - MSAA
 
@@ -41,7 +44,7 @@ namespace Engine.Gui {
             beginFrame();
             text(vec2.zero, Font.arial, 16, "fps: " + Renderer.fps, in color.white);
 
-
+            /*
             if (Keyboard.isPressed(key.G)) initialSeed++;
             int seed = initialSeed;       
 
@@ -74,11 +77,25 @@ namespace Engine.Gui {
             text(vec2.zero, Font.arial, (int)(height), "Text", color.rgb(1, 1, 0));
 
             var c = math.range(seed++, 100, 1000);
-            text((0, 16), Font.arial, 16, "count: " + c, in color.white);
+            text((0, 16), Font.arial, 16, "count: " + c, in color.white);\
             for (int i = 0; i < c; i++) {
                 text((math.range(seed++, 0, width), math.range(seed++, 0, height)), Font.arial, (int)math.range(seed++, 10, 30), math.pick(seed++, a), math.pick(seed++, colors));
 
             }
+
+
+            var s = new vec2(width, height) / 10f;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    var pos = s * new vec2(i, j);
+                    rect(pos, s, color.rgba(i / 10f, j / 10f, pos.distTo(Mouse.position) / 1000f, 0.8f));
+                    text(pos, Font.arial, 22, pos.ToString(), in color.silver);
+                }
+            }
+            */
+
+            rect(150, 100, color.silver);
+            rect(100, 100, color.white);
 
 
             endFrame();
@@ -90,28 +107,27 @@ namespace Engine.Gui {
                 batch.Value.data.clear();
             }
 
+            // clear rect batch
+            rectBatch.data.clear();
+
             // update camera projection
             OpenTK.Mathematics.Matrix4.CreateOrthographic(width, height, 0, 10, out OpenTK.Mathematics.Matrix4 res);
             var p = res.toNums();
             Renderer.updateCameraProjection(ref p);
 
-            // temporarly set uniforms
-            /*var one = vec2.one;
-            var zero = vec2.zero;
-            var c = color.white;
-            GL.Uniform2(GL.GetUniformLocation(Renderer.textShader.id, "size"), 1, ref one.x);
-            GL.Uniform2(GL.GetUniformLocation(Renderer.textShader.id, "pos"), 1, ref zero.x);
-            GL.Uniform4(GL.GetUniformLocation(Renderer.textShader.id, "color"), 1, ref c.red);
-            */
         }
 
         public void endFrame() {
+            Renderer.whiteTexture.bind(TextureUnit.Texture0);
+            rectBatch.updateBuffers();
+            rectBatch.render();
+
+
             foreach (var batch in textBatches) {
                 batch.Key.atlas.bind(OpenTK.Graphics.OpenGL4.TextureUnit.Texture0);
                 batch.Value.updateBuffers();
                 batch.Value.render();
             }
-            
         }
 
         public void text(vec2 pos, Font font, int fontSize, string text, in color color) {
@@ -128,7 +144,38 @@ namespace Engine.Gui {
             pos.y = -pos.y;
 
 
-            
+            size.y = -size.y;
+
+            color.color2vec(in color, out vec4 vertcolor);
+
+            uint count = (uint)rectBatch.data.vertices.Count;
+
+            rectBatch.data.vertices.Add(new textVertex {
+                color = vertcolor,
+                position = pos
+            });
+
+            rectBatch.data.vertices.Add(new textVertex {
+                color = vertcolor,
+                position = pos + (size.x, 0)
+            });
+
+            rectBatch.data.vertices.Add(new textVertex {
+                color = vertcolor,
+                position = pos + size
+            });
+
+            rectBatch.data.vertices.Add(new textVertex {
+                color = vertcolor,
+                position = pos + (0, size.y)
+            });
+
+
+            rectBatch.data.addTriangles(new uint[] {
+                count + 0, count + 2, count + 1,
+                count + 0, count + 3, count + 2
+            });
+
         }
 
     }
