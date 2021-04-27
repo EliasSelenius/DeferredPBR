e400394c19568925fff5ea985b0c9dcff5df96af
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-04-28 01:45:22 +0200

    upt draft

--- Engine/UBO.cs
+++ Engine/UBO.cs
@@ -1,13 +1,17 @@
-
+using System.Collections.Generic;
 using OpenTK.Graphics.OpenGL4;
 
 namespace Engine {    
     public class UBO {
+        
         public readonly int id;
         public readonly int bindingPoint;
         public readonly string name;
 
         static int bindingPoint_count = 0;
+
+        static Dictionary<string, UBO> UBOs = new();
+        public static UBO get(string name) => UBOs[name];
 
         public UBO(string name, int bytesize) {
             this.name = name;
@@ -16,5 +20,6 @@ namespace Engine {
 
             GL.BindBufferBase(BufferRangeTarget.UniformBuffer, bindingPoint, id);
         }
+
     }
 }
--- Engine/data/shaders/Camera.glsl
+++ Engine/data/shaders/Camera.glsl
@@ -1,4 +1,6 @@
 
+#pragma UBO Camera bind
+
 layout (std140) uniform Camera {
     mat4 view;
     mat4 projection;
