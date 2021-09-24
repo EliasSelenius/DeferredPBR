fa4cbc6941b967c0d5d5331359e64922c74f8c98
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-09-24 22:23:42 +0200

    moved stuff around

--- Engine/Gui/ImmediateGui.cs
+++ Engine/Gui/ImmediateGui.cs
@@ -67,6 +67,28 @@ namespace Engine.Gui {
 
             pop();
         }
+
+        public static void number(vec2 pos, ref float value) => throw new NotImplementedException();
+        public static void number(vec2 pos, ref int value) => throw new NotImplementedException();
+
+#region dropdown
+
+        static bool dropdown_is_open = false;
+        static Type dropdown_enum = null;
+        static vec2 dropdown_pos;
+
+        public static void dropdown<T>(vec2 pos, ref T value) where T : Enum {
+            var type = typeof(T);
+            var options = type.GetEnumNames();
+
+            
+            for (int i = 0; i < options.Length; i++) {
+                
+            }
+        }
+
+#endregion
+
     }
 
 
@@ -90,42 +112,5 @@ namespace Engine.Gui {
 
             pop();
         }
-
-        /*public void checkbox(vec2 pos, ref bool value, float size = 10) {
-            var mpos = Mouse.position;
-            float borderThiccnes = 2;
-            rectborder(pos, size, borderThiccnes, in color.gray);
-            if (value) {
-                rect(pos + borderThiccnes, size - borderThiccnes*2, in color.white);
-            }
-
-            if (Utils.insideBounds(mpos - pos, size) && Mouse.isPressed(MouseButton.left)) {
-                value = !value;
-            }
-        }*/
-
-#region dropdown
-
-        bool dropdown_is_open = false;
-        Type dropdown_enum = null;
-        vec2 dropdown_pos;
-
-        public void dropdown<T>(vec2 pos, ref T value) where T : Enum {
-            var type = typeof(T);
-            var options = type.GetEnumNames();
-
-            
-            for (int i = 0; i < options.Length; i++) {
-                
-            }
-        }
-
-#endregion
-
-        //public void slider(vec2 pos, ref float value) => throw new NotImplementedException();
-        //public void slider(vec2 pos, ref int value) => throw new NotImplementedException();
-
-        public void number(vec2 pos, ref float value) => throw new NotImplementedException();
-        public void number(vec2 pos, ref int value) => throw new NotImplementedException();
     }
 }
